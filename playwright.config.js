0d03f6a7e228e4511cd8add152baa6e8803c9382
hendisantika <hendisantika@yahoo.co.id>
2024-06-14 13:28:35 +0700

    Add more details in README.md file🫸🌀✏️📗🐧🐳

--- playwright.config.js
+++ playwright.config.js
@@ -12,6 +12,15 @@ const {defineConfig, devices} = require('@playwright/test');
  */
 module.exports = defineConfig({
     testDir: './tests',
+    /* Maximum time one test can run for. */
+    timeout: 30 * 1000,
+    expect: {
+        /**
+         * Maximum time expect() should wait for the condition to be met.
+         * For example in `await expect(locator).toHaveText();`
+         */
+        timeout: 5000
+    },
     /* Run tests in files in parallel */
     fullyParallel: true,
     /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -24,6 +33,8 @@ module.exports = defineConfig({
     reporter: 'html',
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
+        /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
+        actionTimeout: 0,
         /* Base URL to use in actions like `await page.goto('/')`. */
         baseURL: 'https://restful-booker.herokuapp.com',
 
--- tests/02_post_static_json_data.spec.js
+++ tests/02_post_static_json_data.spec.js
@@ -10,8 +10,8 @@ test('should be able to create a booking', async ({request}) => {
     expect(response.ok()).toBeTruthy();
     expect(response.status()).toBe(200);
     const responseBody = await response.json()
-    expect(responseBody.booking).toHaveProperty("firstname", "Jim");
-    expect(responseBody.booking).toHaveProperty("lastname", "Brown");
+    expect(responseBody.booking).toHaveProperty("firstname", "Itadori");
+    expect(responseBody.booking).toHaveProperty("lastname", "Yuji");
     expect(responseBody.booking).toHaveProperty("totalprice", 111);
     expect(responseBody.booking).toHaveProperty("depositpaid", true);
 });
